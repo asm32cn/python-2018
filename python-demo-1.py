b1d04281e3341b6f13e71242ec9e9542031aad1a
asm32 <asm32@qq.com>
2018-04-24 17:07:17 +0800

    init demo

---
+++ python-demo-1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Python 2.7
+
+import urllib
+
+print urllib.urlopen('http://localhost/').read()
+
+print 'Python 2.7'
+print 'change this line'
+print 'Done.'
---
+++ python-demo-2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Python 2.7
+
+print 'Python 2.7'
