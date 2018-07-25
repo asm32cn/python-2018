db1ad99276f456d7e9b19d28a2432a77dd6b3bc1
asm32cn <asm32@qq.com>
2018-07-25 13:51:15 +0800

    py-base64-demo-1.py ... modify

--- Python-Arithmetic-2018/Python-Arithmetic-2018-encrypt/py-base64-demo-1.py/py-base64-demo-1.py
+++ Python-Arithmetic-2018/Python-Arithmetic-2018-encrypt/py-base64-demo-1.py/py-base64-demo-1.py
@@ -93,6 +93,12 @@ strData:
 程序中书写着所见所闻所感，编译着心中的万水千山。
 strSysEncrypt:
 	cHktYmFzZTY0LWRlbW8tMS5weQrnqIvluo/kuK3kuablhpnnnYDmiYDop4HmiYDpl7vmiYDmhJ/vvIznvJbor5HnnYDlv4PkuK3nmoTkuIfmsLTljYPlsbHjgII=
+strSysDecrypt:
+	py-base64-demo-1.py
+程序中书写着所见所闻所感，编译着心中的万水千山。
 strUserEncrypt:
 	cHktYmFzZTY0LWRlbW8tMS5weQrnqIvluo/kuK3kuablhpnnnYDmiYDop4HmiYDpl7vmiYDmhJ/vvIznvJbor5HnnYDlv4PkuK3nmoTkuIfmsLTljYPlsbHjgII=
+strUserDecrypt:
+	py-base64-demo-1.py
+程序中书写着所见所闻所感，编译着心中的万水千山。
 '''
